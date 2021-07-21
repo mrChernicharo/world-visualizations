c878a59d616ae99e64c856278c15afe716ebcf6f
Felipe Chernicharo <string7dev@gmail.com>
2021-07-20 21:38:45 -0400

    fetching data from GOV API

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,28 +8,18 @@ import { useStore } from '../hooks/useStore';
 
 interface IProps {
   countries: Country[];
+  fuelData: any;
   isAppleM1: boolean;
 }
 
-export async function getStaticProps() {
-  const req = await fetch('https://restcountries.eu/rest/v2/all');
-
-  const countries = await req.json();
-
-  return {
-    props: {
-      countries,
-      isAppleM1: process.arch === 'arm64' && process.platform === 'darwin', // bug fix pro M1
-    },
-  };
-}
-
-export default function Index({ countries, isAppleM1 }: IProps) {
+export default function Index({ countries, fuelData, isAppleM1 }: IProps) {
   const { setCountries } = useStore();
 
   useEffect(() => {
     setCountries(countries);
   }, [setCountries, countries]);
+
+  useEffect(() => console.log(fuelData), [fuelData]);
   return (
     <PageContainer>
       <Nav />
@@ -37,4 +27,23 @@ export default function Index({ countries, isAppleM1 }: IProps) {
       <HomePage countries={countries} isAppleM1 />
     </PageContainer>
   );
+}
+
+export async function getStaticProps() {
+  const req = await fetch('https://restcountries.eu/rest/v2/all');
+
+  const fuelReq = await fetch(
+    `https://developer.nrel.gov/api/alt-fuel-stations/v1.json?limit=10&api_key=${process.env.DATA_GOV_API_KEY}`
+  );
+
+  const countries = await req.json();
+  const fuelData = await fuelReq.json();
+
+  return {
+    props: {
+      countries,
+      fuelData,
+      isAppleM1: process.arch === 'arm64' && process.platform === 'darwin', // bug fix pro M1
+    },
+  };
 }
